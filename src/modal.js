455afb88918a0d3ab2eb99dc0c0c04a8b7ae4b0b
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2015-06-25 15:35:38 -0700

    fixed jscs error

--- src/modal.js
+++ src/modal.js
@@ -73,7 +73,7 @@ Modal.prototype.init = function() {
   }
 
   if (closeButton) {
-    closeButton.addEventListener('click', function () {
+    closeButton.addEventListener('click', function() {
       self.hide();
     });
   }
--- tasks/dev.js
+++ tasks/dev.js
@@ -3,12 +3,18 @@
 
 module.exports = function(grunt) {
 
-  grunt.registerTask('dev', 'watches for file changes and triggers build', function() {
+  grunt.registerTask('dev', 'watches for changes & triggers build', function() {
 
     grunt.initConfig({
       watch: {
         js: {
-          files: ['tasks/**/*.js', 'src/**/*.js', 'test/**/*.js', 'example/**/*.js', '!example/script.js' ],
+          files: [
+            'tasks/**/*.js',
+            'src/**/*.js',
+            'test/**/*.js',
+            'example/**/*.js',
+            '!example/script.js'
+          ],
           tasks: ['js'],
           options: {
             livereload: true
